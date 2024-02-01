7b741846d7d7051f6794805a5ed0f675f20c2ae9
Ruhulcse <ruhul.cse7862@gmail.com>
2024-02-02 00:44:33 +0600

    ïp info added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 const db = require("./db/db");
 const logger = require("morgan");
 const helmet = require("helmet");
@@ -7,10 +8,13 @@ const routes = require("./routes");
 const auth = require("./middlewares/auth");
 const errorHandler = require("./middlewares/errors");
 const cors = require("cors");
-require("./helpers/create_admin");
+const uuid = require("uuid");
+const { lookup } = require("geoip-lite");
 
+require("./helpers/create_admin");
 require("dotenv").config();
 
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -23,19 +27,26 @@ app.use("/public", express.static("public"));
 app.use(errorHandler);
 
 app.get("/", function (req, res) {
-  // console.log(req);
-  const ipInfo = req.ipInfo;
-  console.log("ipinfo ", ipInfo);
   const ipAddress = req.socket.remoteAddress;
   console.log(ipAddress);
-  var message = `Hey, you are browsing from`;
-  res.send(message);
+  // console.log(lookup(ipAddress)); // location of the user
+  // Check if the visitorId cookie exists
+  if (req.cookies.visitorId) {
+    console.log("Returning visitor with ID:", req.cookies.visitorId);
+    res.send("Welcome back, visitor!");
+  } else {
+    // Assign a unique ID and set it in a cookie
+    const visitorId = uuid.v4();
+    res.cookie("visitorId", visitorId, { maxAge: 24 * 60 * 60 * 1000 }); // Expires in 1 day
+    console.log("New visitor assigned ID:", visitorId);
+    res.send("Hello, new visitor!");
+  }
 });
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`server listening on http://127.0.0.1:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`server listening :${PORT}`);
 });
 
 module.exports = app;
